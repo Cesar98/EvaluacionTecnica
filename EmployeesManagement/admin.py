58cff28b45d63adaca8a489503647cbfacbcfd24
cesar alvarez <cesar_zuco@hotmail.com>
2021-07-24 21:40:42 -0500

    Aspectos finales

--- EmployeesManagement/admin.py
+++ EmployeesManagement/admin.py
@@ -4,12 +4,13 @@ from EmployeesManagement.models import Empresas, Departamentos, Empleados
 class EmpleadosAdmin(admin.ModelAdmin):
     list_display=('nombre', 'fecha_nacimiento', 'email', 'genero', 'telefono', 'celular', 'fecha_ingreso', 'departamento')
     list_filter=('nombre', 'fecha_ingreso', 'departamento')
+    search_fields=('nombre', )
     date_hierarchy=('fecha_ingreso')
 
 class DepartamentosAdmin(admin.ModelAdmin):
     list_display=('departamento', 'empresa')
     list_filter=('empresa',)
-    
+
 admin.site.register(Departamentos, DepartamentosAdmin)
 admin.site.register(Empleados, EmpleadosAdmin)
 admin.site.register(Empresas)
